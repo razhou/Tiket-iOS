cfd9d0a2523e7651d5ff72b1d32a45f820c0b4a8
Raju <r4ju.riy4nd4@gmail.com>
2020-04-21 22:22:04 +0700

    Initial Commit

---
+++ Tiket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tiket
+//
+//  Created by Jelajah Data Semesta on 21/04/20.
+//  Copyright © 2020 Raju Riyanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
