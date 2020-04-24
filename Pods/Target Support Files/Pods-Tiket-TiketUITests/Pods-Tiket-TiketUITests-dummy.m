45da863637f103d2372da5fb09a5a55d6247944a
Raju <r4ju.riy4nd4@gmail.com>
2020-04-24 09:50:40 +0700

    - Remove pod lib for low size

--- Pods/Target Support Files/Pods-Tiket-TiketUITests/Pods-Tiket-TiketUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tiket_TiketUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Tiket_TiketUITests
-@end
--- Pods/Target Support Files/Pods-TiketTests/Pods-TiketTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TiketTests : NSObject
-@end
-@implementation PodsDummy_Pods_TiketTests
-@end
